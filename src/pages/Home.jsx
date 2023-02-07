aaef64197e2aa29752f983bfacec0b6c41be8e2c
unknown <w8cctxl@gmail.com>
2023-02-07 15:44:18 +0200

    Upload files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setCategoryId } from '../redux/slices/filterSlice';
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
+import { SearchContext } from '../App';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { categoryId, sort } = useSelector((state) => state.filter);
+
+  console.log('id category', categoryId);
+
+  const { searchValue } = React.useContext(SearchContext);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const onClickCategory = (id) => {
+    dispatch(setCategoryId(id));
+  };
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sort.sortProperty.replace('-', '');
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
+
+    axios
+      .get(
+        `https://63c9892c904f040a96605c5f.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+      )
+      .then((response) => {
+        setItems(response.data);
+        setIsLoading(false);
+      });
+
+    window.scrollTo(0, 0);
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+  return (
+    <>
+      <div className="container">
+        <div className="content__top">
+          <Categories value={categoryId} onClickCategory={onClickCategory} />
+          <Sort />
+        </div>
+        <h2 className="content__title">Всі піцци</h2>
+        <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
+    </>
+  );
+};
+
+export default Home;
